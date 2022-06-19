9824d149cc8e3c3208508605980b3034d78ad335
SaidErdmOzturk <saiderdemozturk@gmail.com>
2022-06-19 05:13:56 +0300

    Add project files.

---
+++ SingletonCalisam/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SingletonCalisam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+     class Singleton
+    {
+        private static Singleton singleton;
+        private Singleton()
+        {
+            //sınıfımızı new yapmamızı engelliyor
+        }
+        public static Singleton olustur()
+        {
+            if (singleton==null)
+            {
+                singleton = new Singleton();
+            }
+            return singleton;
+        }
+    }
+}
